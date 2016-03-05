982e1923118a82584f53f086841c726a71bc49be
Eugene Kondrashev <eugene.kondrashev@gmail.com>
2016-03-05 12:16:16 +0200

    Enabled field modification capability

---
+++ src/main/c/jti_field_modify_to_java.c
@@ -0,0 +1,107 @@
+#include <stdlib.h>
+#include "jvmti.h"
+#include <stdarg.h>
+
+
+jvmtiEnv *globalJVMTIInterface;
+
+void JNICALL
+vmInit(jvmtiEnv * jvmti_env, JNIEnv * jni_env, jthread thread)
+{
+
+  printf("C:\tVMInit, preparing to callback Java method\n");
+
+  char *className = "ek/JVMTICallback";
+  char *methodName = "VMInit";
+  char *descriptor = "()V";
+
+  jclass callbackClass = (*jni_env)->FindClass(jni_env, className);
+
+  if (!callbackClass) {
+      fprintf(stderr,"C:\tUnable to locate callback class.\n");
+      return;
+      }
+  printf("C:\tClass found\n");
+
+  jmethodID callbackMethodID = (*jni_env)->GetStaticMethodID(jni_env, callbackClass, methodName, descriptor);
+
+  if (!callbackMethodID)
+    {
+      fprintf(stderr, "C:\tUnable to locate callback VMInit method\n");
+      return;
+    }
+  printf("C:\tMethod found\n");
+
+  va_list empty_va_list;
+  (*jni_env)->CallStaticVoidMethod(jni_env, callbackClass, callbackMethodID);
+
+  printf("C:\tVMInit, callback Java method returned successfully\n");
+
+
+}
+
+JNIEXPORT jint JNICALL
+Agent_OnLoad(JavaVM * jvm, char *options, void *reserved)
+{
+
+  jint returnCode = (*jvm)->GetEnv(jvm, (void **) &globalJVMTIInterface,
+      JVMTI_VERSION_1_0);
+
+  if (returnCode != JNI_OK)
+    {
+      fprintf(stderr,
+          "The version of JVMTI requested (1.0) is not supported by this JVM.\n");
+      return JVMTI_ERROR_UNSUPPORTED_VERSION;
+    }
+
+  jvmtiCapabilities *capa;
+  capa = calloc(1, sizeof(jvmtiCapabilities));
+  if (!capa)
+   {
+       fprintf(stderr, "Unable to allocate memory\n");
+       return JVMTI_ERROR_OUT_OF_MEMORY;
+   }
+  capa->can_generate_field_modification_events = 1;
+  returnCode = (*globalJVMTIInterface)->AddCapabilities(globalJVMTIInterface, capa);
+  if (returnCode != JNI_OK)
+          {
+            fprintf(
+                stderr,
+                "C:\tJVM does not have the required capabilities, d(%d)\n",
+                returnCode);
+            exit(-1);
+          }
+
+  jvmtiEventCallbacks *eventCallbacks;
+
+  eventCallbacks = calloc(1, sizeof(jvmtiEventCallbacks));
+  if (!eventCallbacks)
+    {
+      fprintf(stderr, "Unable to allocate memory\n");
+      return JVMTI_ERROR_OUT_OF_MEMORY;
+    }
+
+  eventCallbacks->VMInit = &vmInit;
+
+  returnCode = (*globalJVMTIInterface)->SetEventCallbacks(globalJVMTIInterface,
+      eventCallbacks, (jint) sizeof(*eventCallbacks));
+  if (returnCode != JNI_OK)
+    {
+      fprintf(stderr, "C:\tJVM does not have the required capabilities (%d)\n",
+          returnCode);
+      exit(-1);
+    }
+
+  returnCode = (*globalJVMTIInterface)->SetEventNotificationMode(
+      globalJVMTIInterface, JVMTI_ENABLE, JVMTI_EVENT_VM_INIT, (jthread) NULL);
+  if (returnCode != JNI_OK)
+    {
+      fprintf(
+          stderr,
+          "C:\tJVM does not have the required capabilities, JVMTI_ENABLE, JVMTI_EVENT_VM_INIT (%d)\n",
+          returnCode);
+      exit(-1);
+    }
+
+  return JVMTI_ERROR_NONE;
+}
